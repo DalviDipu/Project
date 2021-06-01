d9ec447d5bf0316a521721887ceefc999d120b3e
Dipali Dalvi <Dipali Dalvi@LAPTOP-2DLJGBV4>
2021-05-31 21:35:08 -0700

    some changes on code

--- Course-Management-System/src/com/rsd/commons/SendMail.java
+++ Course-Management-System/src/com/rsd/commons/SendMail.java
@@ -3,6 +3,6 @@ package com.rsd.commons;
 public class SendMail {
 	public static void setMail(String mail,String sub,String msg) {
 		//from,password,to,subject,message  
-	     Mail.send("saraswaticollege0102@gmail.com","Akola@123",mail,sub,msg);  
+	     Mail.send("Mail","Password",mail,sub,msg);  
 	}   
 }
